992e0b5677cfbe9fb52138520b371a824c1ac60e
Streeterxs <afonso_mg3@hotmail.com>
2021-05-30 20:12:14 -0300

    feat(partial-dumper-config): adding config file for partial dumper

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,6 @@ module.exports = {
     plugins: [
         // https://github.com/parcel-bundler/parcel/issues/871#issuecomment-370135105
         // https://github.com/babel/babel-loader/issues/560#issuecomment-370180866
-        '@babel/plugin-transform-runtime',
         '@babel/plugin-transform-modules-commonjs'
     ]
 }
---
+++ partial-dumper.config.ts
@@ -0,0 +1,24 @@
+import Dog from './src/models/DogModel';
+import DogOwner from './src/models/DogOwnerModel';
+import DogToy from './src/models/DogToyModel';
+import Person from './src/models/PersonModel';
+import PetShopClient from './src/models/PetShopClientModel';
+import PetShop from './src/models/PetShopModel';
+import User from './src/models/UserModel';
+
+import config from './src/config';
+
+export default {
+    db: {
+        url: config.db_url
+    },
+    models: [
+        Dog,
+        DogOwner,
+        DogToy,
+        Person,
+        PetShopClient,
+        PetShop,
+        User,
+    ],
+};
